9e30a6b558f7145b97636e9862b98be5fe5e9142
Bryan <bryancalupitan@yahoo.com>
2018-11-04 11:31:12 +0900

    Add news api lib and get top headlines

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.conf import settings
+from newsapi import NewsApiClient
 
 
 class Article(models.Model):
@@ -14,3 +18,39 @@ class Article(models.Model):
 
     class Meta:
         ordering = ("-published_at",)
+
+    @classmethod
+    def get_top_headlines(cls):
+        newsapi = NewsApiClient(api_key=settings.NEWS_API_KEY)
+        top_headlines = newsapi.get_top_headlines(language='en', country='us')
+        articles = top_headlines.get('articles', [])
+        news_headlines = []
+        batch_size = top_headlines.get('totalResults', 20)
+
+        for article in articles:
+            source = article['source']
+            source_id = source['id']
+            source_name = source['name']
+            author = article['author']
+            title = article['title']
+            description = article['description']
+            url = article['url']
+            url_to_image = article['urlToImage']
+            published_at = datetime.datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+
+            news_article = cls(
+                source_id=source_id,
+                source_name=source_name,
+                author=author,
+                title=title,
+                description=description,
+                url=url,
+                url_to_image=url_to_image,
+                published_at=published_at,
+            )
+
+            news_headlines.append(news_article)
+
+        return cls.objects.bulk_create(news_headlines, batch_size)
+
+
