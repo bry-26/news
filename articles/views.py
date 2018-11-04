7afdfbd17dd0e2931767f4477a512f8742880a6f
Bryan <bryancalupitan@yahoo.com>
2018-11-04 11:54:47 +0900

    Article list view

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic.list import ListView
 
-# Create your views here.
+from articles.models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+    queryset = Article.objects.filter()[:20]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
