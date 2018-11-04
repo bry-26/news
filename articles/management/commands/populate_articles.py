58e9d202ad8a83e3e750d61dafd742fda0ead676
Bryan <bryancalupitan@yahoo.com>
2018-11-04 12:11:44 +0900

    Populate article command

--- articles/management/commands/populate_articles.py
+++ articles/management/commands/populate_articles.py
@@ -1,9 +1,16 @@
-from django.core.management.base import BaseCommand, CommandError
-from .models import Articles
+import time
+
+from django.core.management.base import BaseCommand
+from articles.models import Article
 
 
 class Command(BaseCommand):
     help = 'Populate articles fron news api top headlines.'
 
     def handle(self, *args, **options):
-        pass
+        counter = 10
+
+        while counter != 0:
+            Article.get_top_headlines()
+            time.sleep(1)
+            counter -= 1
