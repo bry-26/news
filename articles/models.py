87070797a0f6e06aab33932c51e5e0a5be4b8b1f
Bryan <bryancalupitan@yahoo.com>
2018-11-04 09:32:16 +0900

    Articles model and migration

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    source_id = models.SlugField(max_length=80, null=True, blank=True)
+    source_name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    url = models.URLField()
+    url_to_image = models.URLField()
+    published_at = models.DateTimeField()
+    content = models.TextField()
+
+    class Meta:
+        ordering = ("-published_at",)
